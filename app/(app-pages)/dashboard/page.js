e7afdeed4815577c9fc290c192653eec89ea2d28
Okezie Arukwe <kezyroan@gmail.com>
2025-02-25 14:34:04 +0100

    dashboard

--- app/(app-pages)/dashboard/page.js
+++ app/(app-pages)/dashboard/page.js
@@ -1,24 +1,8 @@
-import Card from "@/components/organisms/Card/Card"
-import TableOne from "@/components/organisms/Table/BasicTableOne"
-import Pagination from "@/components/organisms/Table/Pagination"
-import SummaryCard from "@/components/organisms/SummaryCard/SummaryCard"
+import Dashboard from "@/components/pages/Dashboard/Dashboard"
 
 export default function HomePage() {
 
   return (
-    <div>
-      <Card className="mb-6">
-        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-4">
-          <SummaryCard title="Unique Visitors" value="24.7K" percentage="+20%" comment="Vs last month" />
-          <SummaryCard title="Unique Visitors" value="24.7K" percentage="+20%" comment="Vs last month" />
-          <SummaryCard title="Unique Visitors" value="24.7K" percentage="+20%" comment="Vs last month" />
-          <SummaryCard title="Unique Visitors" value="24.7K" percentage="+20%" comment="Vs last month" />
-        </div>
-      </Card>
-      <Card title="Table One" desc="Example of a table">
-        <TableOne />
-        <Pagination />
-      </Card>
-    </div>
+    <Dashboard />
   )
 }
--- app/_lib/components/pages/Dashboard/Dashboard.js
+++ app/_lib/components/pages/Dashboard/Dashboard.js
@@ -1,8 +1,23 @@
+import Card from "@/components/organisms/Card/Card"
+import TableOne from "@/components/organisms/Table/BasicTableOne"
+import Pagination from "@/components/organisms/Table/Pagination"
+import SummaryCard from "@/components/organisms/SummaryCard/SummaryCard"
 
 export default function Dashboard() {
     return (
         <div>
-            <h1>Dashboard</h1>
+            <Card className="mb-6">
+                <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-4">
+                    <SummaryCard title="Unique Visitors" value="24.7K" percentage="+20%" comment="Vs last month" />
+                    <SummaryCard title="Unique Visitors" value="24.7K" percentage="+20%" comment="Vs last month" />
+                    <SummaryCard title="Unique Visitors" value="24.7K" percentage="+20%" comment="Vs last month" />
+                    <SummaryCard title="Unique Visitors" value="24.7K" percentage="+20%" comment="Vs last month" />
+                </div>
+            </Card>
+            <Card title="Table One" desc="Example of a table">
+                <TableOne />
+                <Pagination />
+            </Card>
         </div>
     )
 }
