7490f5d1f87d17cd65e3b04a6225b705e44aaea4
Okezie Arukwe <kezyroan@gmail.com>
2025-04-13 11:23:00 +0100

    fix build

--- app/(auth)/logout/page.js
+++ app/(auth)/logout/page.js
@@ -1,7 +1,12 @@
 import { logout } from "@/actions/actionUtils"
 import { Constants } from "@/utils/Constants"
+import { redirect } from "next/navigation"
 
-export default async function Logout() {
-    await logout()
-    window?.location?.href = Constants.Paths.SignIn
+export default function Logout() {
+    logout().then(() => {
+        if (window?.location?.href){
+            window.location.href = Constants.Paths.SignIn
+        }
+        else redirect(Constants.Paths.SignIn)
+    })
 }
